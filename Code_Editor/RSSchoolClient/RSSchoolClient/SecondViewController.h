6ba5fc76df19fed2581c8072a58d63b1184f0aa3
Vi Jane <creative.mashine@gmail.com>
2021-05-06 21:48:03 +0300

    feat: add all files of project RSSchoolClient in the new branch Code_Editor for new theme Code_Editor

---
+++ Code_Editor/RSSchoolClient/RSSchoolClient/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  RSSchoolClient
+//
+//  Created by Viktoriia Jane Ro (Viktoriia Romaniuk) on 06.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Code_Editor/RSSchoolClient/RSSchoolClient/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  RSSchoolClient
+//
+//  Created by Viktoriia Jane Ro (Viktoriia Romaniuk) on 06.05.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
